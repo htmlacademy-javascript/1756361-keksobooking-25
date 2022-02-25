394eb0a1f6a17de9a854ef226e9ee25c3e96c59a
Keks <keks@htmlacademy.ru>
2022-02-25 14:42:57 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,17 +1,16 @@
 const getRandomInteger = (min, max) => {
   if (max <= min) {
-    return(`Максимальное ${max} число не может быть меньше или равно ${min}`);
-
+    throw new Error(`Максимальное ${max} число не может быть меньше или равно ${min}`);
   }
-  return Math.floor(min + Math.random() * (max - min));
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const getRandomFloat = (min, max) => {
+const getRandomFloat = (min, max, count) => {
   if (max <= min) {
-    return(`Максимальное ${max} число не может быть меньше или равно ${min}`);
+    throw new Error(`Максимальное ${max} число не может быть меньше или равно ${min}`);
   }
-  return (min + Math.random() * (max - min)).toFixed(5);
+  return (min + Math.random() * (max - min)).toFixed(count);
 };
 
 getRandomInteger(1,2);
-getRandomFloat(1,10);
+getRandomFloat(1,10, 5);
