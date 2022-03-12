09f6c1dc4729cd72c0a48d3f906a16393d70690a
Keks <keks@htmlacademy.ru>
2022-03-12 19:23:57 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -13,7 +13,7 @@ const getRandomFloat = (min, max, count) => {
 };
 
 const shuffleArray = (array) => {
-  for (let i = array.length - 1; i > 0;) {
+  for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     const temp = array[i];
     array[i] = array[j];
