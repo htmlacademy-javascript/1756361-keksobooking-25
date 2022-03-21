0d80a872f047f58955ded1fddc1f18a98139c6e6
Keks <keks@htmlacademy.ru>
2022-03-22 00:24:48 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,3 @@
+import './generationcard.js';
+
+import './util.js';
---
+++ 5/js/util.js
@@ -0,0 +1,13 @@
+export const getRandomInteger = (min, max) => {
+  if (max <= min) {
+    throw new Error(`Максимальное ${max} число не может быть меньше или равно ${min}`);
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export const getRandomFloat = (min, max, count) => {
+  if (max <= min) {
+    throw new Error(`Максимальное ${max} число не может быть меньше или равно ${min}`);
+  }
+  return (min + Math.random() * (max - min)).toFixed(count);
+};
