8d8e8c3389dc62e812e530afa79a8d268f20cf93
Keks <keks@htmlacademy.ru>
2022-03-25 19:43:24 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,11 @@
+import { makeSimilarAdvertisements } from './generationcard.js';
+import { makePopup } from './card.js';
+
+const adv = makeSimilarAdvertisements();
+const mapCanvas = document.querySelector('#map-canvas');
+
+const element = makePopup(adv[0]);
+mapCanvas.appendChild(element);
+
+import './util.js';
+import './form.js';
