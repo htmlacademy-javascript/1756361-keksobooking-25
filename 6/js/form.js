5d042cd77add21405c14b8145c055561fb32f6d0
Keks <keks@htmlacademy.ru>
2022-03-29 19:13:45 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/form.js
+++ 6/js/form.js
@@ -2,27 +2,27 @@ import {apartmentType} from './card.js';
 
 const form = document.querySelector('.ad-form');
 
-const formMap = document.querySelector('.map__filters');
-
-
-// валидация заголовка
-
-const title = document.querySelector('#title');
-
-const pristine = new Pristine(title, {
-  classTo: 'title',
-  errorClass: 'title--invalid',
-  successClass: 'title--valid',
+//валидация имени
+const pristine = new Pristine(form, {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form--invalid',
+  successClass: 'ad-form--valid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'ad-form__error'
 });
 
-title.addEventListener('submit', (evt) => {
-  evt.preventDefault();
+form.addEventListener('submit',  (e) => {
+  e.preventDefault();
   pristine.validate();
 });
 
-// валидация прайса
+//валидация прайса
 
-const apartmentMinPrice  = {
+const price = document.querySelector('#price');
+const type = document.querySelector('#type');
+
+const typeMinPrice  = {
   palace:10000,
   flat:1000,
   house:5000,
@@ -30,110 +30,68 @@ const apartmentMinPrice  = {
   hotel:3000,
 };
 
-const userTypeSelect = form.querySelector('#type');
-const userPriceSelect = form.querySelector('#price');
+let currentType;
+let currentValidPrice;
 
-const validatePrice = (value) => value >= apartmentMinPrice(userTypeSelect.value);
-
-const getPriceErrorMessage = () => `Минимальная цена для жилья
-'${apartmentType[userTypeSelect.value]}' должна быть '${apartmentMinPrice(userTypeSelect.value)}' руб.`;
-
-pristine.addValidator(
-  validatePrice,
-  getPriceErrorMessage,
-  `Минимальное значение ${apartmentMinPrice}`
-);
-
-pristine.addValidator(
-  validatePrice,
-  getPriceErrorMessage,
-  'Слишком низкая стоимость'
-);
-
-userTypeSelect.addEventListener('change', (evt) => {
-
-  userPriceSelect.placeholder = apartmentMinPrice(evt.target.value);
-  pristine.validate(userPriceSelect);
-});
-
-// валидация комнат
-
-const select1 = [
-  {
-    value: 1,
-    text: '1 комната'
-  }, {
-    value: 3,
-    text: '2 комнаты'
-  }, {
-    value: 3,
-    text: '3 комнаты'
-  }, {
-    value: 100,
-    text: '100 комнат'
-  }
-];
-
-const select2 = [
-  {
-    id: 0,
-    is: [0, 1, 2],
-    text: 'для 1 гостя'
-  }, {
-    id: 1,
-    is: [1, 2],
-    text: 'для 2 гостей'
-  }, {
-    id: 2,
-    is: [2],
-    text: 'для 3 гостей'
-  }, {
-    id: 3,
-    is: [3],
-    text: 'Не для гостей'
-  }
-];
-
-const fieldOne = document.getElementById('#room_number');
-const fieldTwo = document.getElementById('#capacity');
-
-createOptions(fieldOne, select1);
-eventChange.call(fieldOne, fieldTwo);
-
-fieldOne.addEventListener('change', eventChange.bind(fieldOne, fieldTwo));
-
-function eventChange(field) {
-  field.textContent = '';
-  const filtered = select2.filter((o) => o.is.includes(+this.value));
-  createOptions(field, filtered);
-}
-
-
-function createOptions(field, arrO) {
-  for (const o of arrO) {
-    const option = document.createElement('option');
-    option.textContent = o.text;
-    option.setAttribute('value', o.id);
-    field.append(option);
-  }
-}
-
-// неактивное состояние
-
-const createDisable = (element, className) => {
-  element.classList.add(`${className}--disabled`);
-  Array.from(element.elements).forEach ((formElement) => {formElement.disabled = true;});
+const textError = () => {
+  currentValidPrice = typeMinPrice[type.value];
+  currentType = apartmentType[type.value];
+  return `Минимальная цена для ${currentType} ${currentValidPrice}₽.`;
 };
 
-createDisable(form, 'ad-form');
-createDisable(formMap, 'map__filters');
-
-// активное состояние
-
-const createActive = (element, className) => {
-  element.classList.remove(`${className}--disabled`);
-  Array.from(element.elements).forEach((formElement) => {formElement.disabled = false;});
+const priceValid = (userPrice) => {
+  userPrice = true;
+  return typeMinPrice[type.value] <= userPrice; //взять значение, перечень значений соответсвую назначить цену
 };
 
-createActive(form, 'ad-form');
-createActive(formMap, 'map__filters');
+pristine.addValidator(price, priceValid, textError);
+
+// комнаты
+const numberSelector = form.querySelector('#room_number');
+const capacitySelector = form.querySelector('#capacity');
+
+const numberCapacity = {
+  1: ['1'],
+  2: ['2', '1'],
+  3: ['3', '2', '1'],
+  100: ['0']
+};
+
+const validNumberCapasity = () => {
+  if (numberCapacity[numberSelector.value].includes(capacitySelector.value)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+
+pristine.addValidator(capacitySelector, validNumberCapasity, 'Недопустимое количество гостей при заданном количестве комнат');
+
+//время выезда время заезда
+
+const  timeIn = document.querySelector('#timein');
+const timeOut = document.querySelector('#timeout');
+
+const validateTimeError = () => {
+  if (timeIn.value !== timeOut.value){
+    return false;
+  } else {
+    return true;
+  }
+};
+
+pristine.addValidator(timeOut, validateTimeError, 'Время выезда должно соответствовать времени заезда');
+
+// const validateTimeOut = () => {
+//   timeOut.value = timeIn.value;
+//   pristine.validate(timeOut);
+// };
+
+// const validateTimeIn = () => {
+//   timeIn.value = timeOut.value;
+//   pristine.validate(timeOut);
+// };
+
+// validateTimeIn();
+// validateTimeOut();
